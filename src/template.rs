cff9f9526462e2c23d22535ee29b71d9ab1b0c40
MagicPotatoBean <terraboss576@gmail.com>
2024-04-23 10:10:36 +0100

    Update template

--- src/template.rs
+++ src/template.rs
@@ -1,11 +1,12 @@
-use std::{net::TcpStream, io::Write};
+use std::net::TcpStream;
 
-use crate::{FtpResponseCode, FtpState};
+use crate::{FtpState, ftp_methods::FtpCode};
 
 pub fn template(stream: &mut TcpStream, state: &mut FtpState, request: Option<String>) -> Option<()> {
     if state.authenticated {
-
+        todo!()
     } else {
         FtpCode::NotLoggedIn.send(stream, "Invalid username or password").ok()?;
     }
+    Some(())
 }
